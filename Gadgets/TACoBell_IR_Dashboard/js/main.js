e24dd0b023fb08ff20acb5d70af6108e1eb53beb
David Merritt <david.c.merritt@siemens.com>
2014-01-06 09:52:25 +0000

    Added copyright notice and delay launch option

--- Gadgets/TACoBell_IR_Dashboard/js/main.js
+++ Gadgets/TACoBell_IR_Dashboard/js/main.js
@@ -1,3 +1,35 @@
+/*
+ ************************************************************************
+ *
+ * TACoBell_IR_Dashboard
+ * Copyright (C) 2014, David C. Merritt, david.c.merritt@siemens.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ************************************************************************
+ * 
+ * The javascript for the main.html file for a Windows Sidebar gadget to 
+ * display the GTAC EMEA TACoBell IR Dashboard in a minimal window.
+ * 
+ ************************************************************************
+ * 
+ * 03/01/2014  merritt  initial release
+ * 05/01/2014  merritt  added copyright notice and delay launch option
+ * 
+ ************************************************************************
+*/
+
 // enable the gadget settings functionality
 System.Gadget.settingsUI = "settings.html";
 System.Gadget.onSettingsClosed = SettingsClosed;
@@ -32,13 +64,19 @@ if (System.Gadget.Settings.read("ClipDocked") == "")
     System.Gadget.Settings.write("ClipDocked", true);
 }
 
+if (System.Gadget.Settings.read("DelayStart") == "")
+{
+    System.Gadget.Settings.write("DelayStart", 0);
+}
+
+// set the TACoBell url
+var urlTacobell = "http://gbcbygtac01:8080/cgi-bin/tb_main_page.pl";
+var delayCount = System.Gadget.Settings.read("DelayStart") * 60;
 
 // amount of time desired to perform dock transition (in seconds)
 var timeTransition = 2;
 
-// --------------------------------------------------------------------
 // check the gadget dock state and set the gadget style
-// --------------------------------------------------------------------
 function CheckDockState()
 {
     System.Gadget.beginTransition();
@@ -73,9 +111,7 @@ function CheckDockState()
     System.Gadget.endTransition(System.Gadget.TransitionType.morph, timeTransition);
 }
 
-// --------------------------------------------------------------------
 // handle the Settings dialog closed event
-// --------------------------------------------------------------------
 function SettingsClosed(event)
 {
     // user hits OK on the settings page
@@ -84,3 +120,59 @@ function SettingsClosed(event)
         CheckDockState();
     }
 }
+
+// check if the launch should be delayed
+function CheckLaunch()
+{
+    if (delayCount <= 0)
+    {
+        document.getElementById("displayDelay").firstChild.nodeValue = "";        
+        document.getElementById("tacoFrame").src = urlTacobell;
+    }
+    else
+    {
+        intervalId = setInterval(function(){CountDown()}, 1000);            
+    }
+}
+
+
+// calculate hours, mins and secs and display time
+function DisplayTime(now)
+{
+    var hour = Math.floor((now / 3600) % 24);
+    var min = Math.floor((now / 60) % 60);
+    var sec = Math.floor((now / 1) % 60);
+    if (min < 10) 
+    { 
+        min = "0" + min; 
+    }
+    if (sec < 10) 
+    { 
+        sec = "0" + sec; 
+    }
+    if (hour < 10) 
+    { 
+        hour = "0" + hour; 
+    }
+    
+    // set the colour and text
+    document.body.style.backgroundColor = System.Gadget.Settings.read("Background");
+    document.body.style.color = System.Gadget.Settings.read("Colour");
+    document.getElementById("displayDelay").firstChild.nodeValue = "Launching TACoBell in " + hour + ":" + min + ":" + sec;          
+ }
+
+ // the main countdown
+function CountDown()
+{
+    delayCount = delayCount - 1;
+    if (delayCount <= 0)
+    {
+        document.getElementById("displayDelay").firstChild.nodeValue = "Launching TACoBell...";        
+        document.getElementById("tacoFrame").src = urlTacobell;
+        document.getElementById("displayDelay").firstChild.nodeValue = "";        
+        clearInterval(intervalId);
+        return;
+    }
+
+    DisplayTime(delayCount);
+}    
--- Gadgets/TACoBell_IR_Dashboard/js/settings.js
+++ Gadgets/TACoBell_IR_Dashboard/js/settings.js
@@ -1,3 +1,35 @@
+/*
+ ************************************************************************
+ *
+ * TACoBell_IR_Dashboard
+ * Copyright (C) 2014, David C. Merritt, david.c.merritt@siemens.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ************************************************************************
+ * 
+ * The javascript for the settings.html file for a Windows Sidebar gadget
+ * to display the GTAC EMEA TACoBell IR Dashboard in a minimal window.
+ * 
+ ************************************************************************
+ * 
+ * 03/01/2014  merritt  initial release
+ * 05/01/2014  merritt  added copyright notice and delay launch option
+ * 
+ ************************************************************************
+*/
+
 System.Gadget.onSettingsClosing = settingsClosing;
 
 // loadSettings() - loads the stored settings into the settings page form
@@ -8,6 +40,8 @@ function loadSettings()
     WidthUndocked.innerText = System.Gadget.Settings.read("WidthUndocked");
     HeightUndocked.innerText = System.Gadget.Settings.read("HeightUndocked");
     document.getElementById("ClipDocked").checked = System.Gadget.Settings.read("ClipDocked");
+    DelayStart.innerText = System.Gadget.Settings.read("DelayStart");
+
 }
 
 // settingsClosing() - triggered by the System.Gadget.onSettingsClosing event
@@ -28,6 +62,9 @@ function settingsClosing(event)
         {
             System.Gadget.Settings.write("ClipDocked", false);
         }
+        
+        System.Gadget.Settings.write("DelayStart", DelayStart.value);
+
 	}
      
     // allow the Settings dialog to close
